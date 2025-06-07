7d4cc444755d6b46d5d7c7bdaebf8e91564f7ac5
Minhbeo8 <moimoi1112a@gmail.com>
2025-06-07 09:10:33 +0700

    Update buy_cmd.py

--- cogs/shop/buy_cmd.py
+++ cogs/shop/buy_cmd.py
@@ -1,4 +1,3 @@
-# bot/cogs/shop/buy_cmd.py
 import nextcord
 from nextcord.ext import commands
 import logging
@@ -7,34 +6,8 @@ from core.database import (
     load_economy_data,
     get_or_create_global_user_profile,
     get_or_create_global_shop_stock,
-    update_shop_item_stock,
-    save_economy_data
-)
-from core.utils import try_send
-from core.config import CURRENCY_SYMBOL, SHOP_ITEMS, COMMAND_PREFIX
-from core.icons import ICON_SUCCESS, ICON_ERROR, ICON_WARNING, ICON_MONEY_BAG, ICON_INFO
-# bot/cogs/shop/buy_cmd.py
-import nextcord
-from nextcord.ext import commands
-import logging
-
-from core.database import (
-    load_economy_data,
-    get_or_create_global_user_profile,
-    get_or_create_global_shop_stock,
-    update_shop_item_stock,
     get_shop_item_details_from_stock,
-import nextcord
-from nextcord.ext import commands
-import logging
-
-from core.database import (
-    load_economy_data,
-    get_or_create_global_user_profile,
-    get_or_create_user_server_data,
-    get_or_create_global_shop_stock,
     update_shop_item_stock,
-    get_shop_item_details_from_stock,
     save_economy_data
 )
 from core.utils import try_send
@@ -55,9 +28,9 @@ class BuyCommandCog(commands.Cog, name="Buy Command"):
             return
 
         author_id = ctx.author.id
-        guild_id = ctx.guild.id
+        guild_name_for_log = ctx.guild.name
         
-        logger.debug(f"Lệnh 'buy' được gọi bởi {ctx.author.name} ({author_id}) với item_name='{item_name}', quantity={quantity} tại guild '{ctx.guild.name}' ({guild_id}).")
+        logger.debug(f"Lệnh 'buy' được gọi bởi {ctx.author.name} ({author_id}) với item_name='{item_name}', quantity={quantity} tại guild '{guild_name_for_log}'.")
 
         if quantity <= 0:
             await try_send(ctx, content=f"{ICON_ERROR} Số lượng mua phải lớn hơn 0.")
@@ -71,7 +44,6 @@ class BuyCommandCog(commands.Cog, name="Buy Command"):
                 first_word_as_int = int(parts[0])
                 parsed_quantity = first_word_as_int
                 processed_item_name = " ".join(parts[1:])
-                logger.debug(f"Đã phân tích lại input cho 'buy': quantity={parsed_quantity}, item_name='{processed_item_name}'")
             except ValueError:
                 processed_item_name = item_name 
         
@@ -92,7 +64,7 @@ class BuyCommandCog(commands.Cog, name="Buy Command"):
         
         economy_data = load_economy_data()
         user_profile = get_or_create_global_user_profile(economy_data, author_id)
-        user_server_data = get_or_create_user_server_data(user_profile, guild_id)
+        server_data = get_or_create_user_server_data(user_profile, ctx.guild.id)
         shop_stock = get_or_create_global_shop_stock(economy_data)
         item_stock_details = get_shop_item_details_from_stock(shop_stock, item_id_to_buy)
 
@@ -106,10 +78,10 @@ class BuyCommandCog(commands.Cog, name="Buy Command"):
 
         current_stock = item_stock_details.get("current_stock", 0)
         if current_stock < parsed_quantity:
-             await try_send(ctx, content=f"{ICON_ERROR} Rất tiếc, cửa hàng toàn cục chỉ còn **{current_stock}** {item_name_display}. Bạn không thể mua {parsed_quantity} cái.")
+             await try_send(ctx, content=f"{ICON_ERROR} Rất tiếc, cửa hàng toàn cục chỉ còn **{current_stock}** {item_name_display}.")
              return
         
-        local_balance = user_server_data.get("local_balance", {})
+        local_balance = server_data.get("local_balance", {})
         earned_amount = local_balance.get("earned", 0)
         admin_added_amount = local_balance.get("admin_added", 0)
         total_local_balance = earned_amount + admin_added_amount
@@ -121,15 +93,15 @@ class BuyCommandCog(commands.Cog, name="Buy Command"):
         admin_money_spent = min(admin_added_amount, total_price)
         earned_money_spent = total_price - admin_money_spent
         
-        user_server_data["local_balance"]["admin_added"] -= admin_money_spent
-        user_server_data["local_balance"]["earned"] -= earned_money_spent
+        server_data["local_balance"]["admin_added"] -= admin_money_spent
+        server_data["local_balance"]["earned"] -= earned_money_spent
         
         item_source = "admin_added" if admin_money_spent > 0 else "earned"
         
         if item_source == "admin_added":
-            inventory_to_add = user_server_data.setdefault("inventory_local", [])
+            inventory_to_add = server_data.setdefault("inventory_local", [])
             destination_inventory_name = "Túi Đồ Local"
-        else: # item_source == "earned"
+        else: 
             inventory_to_add = user_profile.setdefault("inventory_global", [])
             destination_inventory_name = "Túi Đồ Toàn Cục (GOL)"
 
@@ -140,27 +112,9 @@ class BuyCommandCog(commands.Cog, name="Buy Command"):
         
         save_economy_data(economy_data)
 
-        logger.info(f"Guild: {ctx.guild.name} ({guild_id}) - User: {ctx.author.display_name} ({author_id}) đã mua {parsed_quantity} x '{item_name_display}'. "
-                    f"Giá: {total_price:,}. Tiêu từ 'admin_added': {admin_money_spent:,}, từ 'earned': {earned_money_spent:,}. "
-                    f"Vật phẩm được thêm vào: {destination_inventory_name}.")
+        logger.info(f"User {ctx.author.display_name} ({author_id}) đã mua {parsed_quantity} x '{item_name_display}' với tổng giá {total_price:,} {CURRENCY_SYMBOL}. Tiêu từ 'admin_added': {admin_money_spent:,}, từ 'earned': {earned_money_spent:,}. Vật phẩm được thêm vào: {destination_inventory_name}.")
         
-        await try_send(ctx, content=f"{ICON_SUCCESS} Bạn đã mua thành công **{parsed_quantity} {item_name_display}** với tổng giá **{total_price:,}** {CURRENCY_SYMBOL}! "
-                                    f"Vật phẩm đã được thêm vào **{destination_inventory_name}** của bạn.")
-
-def setup(bot: commands.Bot):
-    bot.add_cog(BuyCommandCog(bot))
-với tổng giá {total_price:,} {CURRENCY_SYMBOL}. "
-                    f"Ví Toàn Cục: {original_global_balance:,} -> {user_profile['global_balance']:,}.")
-        
-        await try_send(ctx, content=f"{ICON_SUCCESS} Bạn đã mua thành công **{parsed_quantity} {item_name_display}** với tổng giá **{total_price:,}** {CURRENCY_SYMBOL}! Chúng đã được thêm vào Túi Đồ Toàn Cục của bạn (`{COMMAND_PREFIX}inv`).")
-        logger.debug(f"Lệnh 'buy' cho {ctx.author.name} đã xử lý xong.")
-
-def setup(bot: commands.Bot):
-    bot.add_cog(BuyCommandCog(bot))
-     f"Ví Toàn Cục: {original_global_balance:,} -> {user_profile['global_balance']:,}.")
-        
-        await try_send(ctx, content=f"{ICON_SUCCESS} Bạn đã mua thành công **{parsed_quantity} {item_name_display}** với tổng giá **{total_price:,}** {CURRENCY_SYMBOL}! Chúng đã được thêm vào Túi Đồ Toàn Cục của bạn (`{COMMAND_PREFIX}inv`).")
-        logger.debug(f"Lệnh 'buy' cho {ctx.author.name} (mua {parsed_quantity} {item_name_display}) đã xử lý xong.")
+        await try_send(ctx, content=f"{ICON_SUCCESS} Bạn đã mua thành công **{parsed_quantity} {item_name_display}** với tổng giá **{total_price:,}** {CURRENCY_SYMBOL}! Chúng đã được thêm vào **{destination_inventory_name}** của bạn.")
 
 def setup(bot: commands.Bot):
     bot.add_cog(BuyCommandCog(bot))
